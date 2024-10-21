f4f71ca1b0bc68b68b852a2bd0f4471c863e2f45
Bunnibo <beatrisraj7@gmail.com>
2024-10-22 00:39:07 +0300

    Added filter buttons toggle and styling

--- src/main.js
+++ src/main.js
@@ -1,3 +1,32 @@
-import '../styles/style.css'
-import '../styles/cards.css'
-import '../styles/utils.css'
+import '../styles/style.css';
+import '../styles/cards.css';
+import '../styles/utils.css';
+
+document.addEventListener('DOMContentLoaded', function(){
+    const buttons = document.querySelectorAll('.card__dropDown_btn');
+    [...buttons].forEach(btn => btn.querySelector('.icon__header').addEventListener('click', dropDown));
+}) //selects the svg and adds an event listener
+
+function dropDown(e){
+    const buttonPar = e.target.closest('.card__dropDown_btn');
+
+    if (!buttonPar) {
+        console.error('Button parent not found or missing data-menu attribute.');
+        return;
+    }
+
+    const menuId = buttonPar.getAttribute('data-menu');
+    const dropdownContainer = document.getElementById(menuId);
+
+    if (!dropdownContainer) {
+        console.error(`Dropdown container with ID ${menuId} not found.`);
+        return;
+    }
+
+    // Toggle the dropdown menu
+    if (dropdownContainer.style.display === 'none' || !dropdownContainer.style.display) {
+        dropdownContainer.style.display = 'flex'; 
+    } else {
+        dropdownContainer.style.display = 'none';  
+    }
+}
