5032519a8617af1d2baefe2c15edf3d6a8ca4418
Bunnibo <beatrisraj7@gmail.com>
2024-10-22 22:55:26 +0300

    Added task functuality for adding simple tasks

--- src/main.js
+++ src/main.js
@@ -1,32 +1,18 @@
 import '../styles/style.css';
 import '../styles/cards.css';
 import '../styles/utils.css';
+import { dropDown } from './buttons.js';
+import { addNewTask } from './buttons.js';
 
+//buttons
+const addNewTaskBtn = document.querySelector('.newTaskBtn');
+
+
+//event Listeners -->
 document.addEventListener('DOMContentLoaded', function(){
     const buttons = document.querySelectorAll('.card__dropDown_btn');
     [...buttons].forEach(btn => btn.querySelector('.icon__header').addEventListener('click', dropDown));
 }) //selects the svg and adds an event listener
+addNewTaskBtn.addEventListener('click', addNewTask)
 
-function dropDown(e){
-    const buttonPar = e.target.closest('.card__dropDown_btn');
 
-    if (!buttonPar) {
-        console.error('Button parent not found or missing data-menu attribute.');
-        return;
-    }
-
-    const menuId = buttonPar.getAttribute('data-menu');
-    const dropdownContainer = document.getElementById(menuId);
-
-    if (!dropdownContainer) {
-        console.error(`Dropdown container with ID ${menuId} not found.`);
-        return;
-    }
-
-    // Toggle the dropdown menu
-    if (dropdownContainer.style.display === 'none' || !dropdownContainer.style.display) {
-        dropdownContainer.style.display = 'flex'; 
-    } else {
-        dropdownContainer.style.display = 'none';  
-    }
-}
