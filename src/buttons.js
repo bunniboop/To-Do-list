250b2d8e3bcd25d7103aa5cb506151201b02b846
Bunnibo <beatrisraj7@gmail.com>
2024-10-28 00:36:08 +0200

    Fixed some bugs and added more button funcuality

--- src/buttons.js
+++ src/buttons.js
@@ -22,8 +22,8 @@ function dropDown(e){
     }
 }
 
-function addNewTask(e){
-    const todoContainer = document.getElementById('task-wrapper-to-do');
+function addNewTask(conteinderid){
+    const todoContainer = document.getElementById(conteinderid);
     const div =  makeElement({type: 'div', className: 'single-task'});
 
     const svg = '<svg viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg" stroke=""><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <circle cx="8" cy="8" r="8" fill="#afb6c9"></circle> </g></svg>';
@@ -39,12 +39,14 @@ function addNewTask(e){
     todoContainer.appendChild(div)
 }
 
-function makeElement({type, content, className, idName}){
+function makeElement({type, content, className, idName, _src, inner}){
     const types = ['div', 'button', 'p', 'a', 'h1', 'h2', 'h3', 'input', ] //add a test for correct type
     const element = document.createElement(type);
     
     content ? element.textContent = content : element.textContent = '';
     className ? element.className = className : element.className = '';
+    if(_src) element.src = _src;
+    if(inner) element.innerHTML = inner;
 
     return element;
 }
@@ -70,8 +72,89 @@ function inputDone(textArea, container){
 
     container.appendChild(taskTitle)
     container.appendChild(btnContainer)
+    const completedContainer = document.getElementById('task-wrapper-copmleted');
+    taskTitle.addEventListener('click', () => {
+        if (!completedContainer.contains(container)){
+            replaceTaskTilte(taskTitle)
+        }
+    })
+    confirmBtn.addEventListener('click', function(e){
+        confirmBtn.remove();
+        cancelBtn.remove()
+        completedContainer.appendChild(container)
+    });
+
+    cancelBtn.addEventListener('click', function(e){
+        container.remove()
+    })
 }
+
+function replaceTaskTilte(taskTitle){
+    const inputArea = makeElement({type:'input',className: 'task_input_area'});
+    taskTitle.replaceWith(inputArea)
+    inputArea.addEventListener('keypress', function(e){
+        if (e.key === 'Enter') replaceInput(taskTitle, inputArea)
+        
+    })
+}
+
+function replaceInput(taskTitle, inputArea ){
+    taskTitle.textContent = inputArea.value;
+    inputArea.replaceWith(taskTitle)
+}
+
+function removeAll(){
+    const completedContainer = document.getElementById('task-wrapper-copmleted');
+    completedContainer.innerHTML = '';
+}
+
+function createGroup(e){
+    const inProgressContainer = document.getElementById('task-wrapper-in-progress');
+    //making elements
+    const divGroup = makeElement({type:'div', className:'group-task-wrapper'});
+    const divGroupHeader = makeElement({type:'div', className:'group-tasks'})
+    const svgDropMenu = makeElement({type:'button',className: 'icon__task_btn', inner:'<svg viewBox="0 0 20 20" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:sketch="http://www.bohemiancoding.com/sketch/ns" fill="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <title>hamburger</title> <desc>Created with Sketch Beta.</desc> <defs> </defs> <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" sketch:type="MSPage"> <g id="Icon-Set-Filled" sketch:type="MSLayerGroup" transform="translate(-212.000000, -888.000000)" fill="#afb6c9"> <path d="M230,904 L214,904 C212.896,904 212,904.896 212,906 C212,907.104 212.896,908 214,908 L230,908 C231.104,908 232,907.104 232,906 C232,904.896 231.104,904 230,904 L230,904 Z M230,896 L214,896 C212.896,896 212,896.896 212,898 C212,899.104 212.896,900 214,900 L230,900 C231.104,900 232,899.104 232,898 C232,896.896 231.104,896 230,896 L230,896 Z M214,892 L230,892 C231.104,892 232,891.104 232,890 C232,888.896 231.104,888 230,888 L214,888 C212.896,888 212,888.896 212,890 C212,891.104 212.896,892 214,892 L214,892 Z" id="hamburger" sketch:type="MSShapeGroup"> </path> </g> </g> </g></svg>'})
+    const textArea = makeElement({type:'input', className: 'task_input_area'});
+    textArea.placeholder ='New group name...';
+    svgDropMenu.setAttribute('toggle', 'closed')
+    svgDropMenu.disabled = true;
+    const divOfTask = makeElement({type:"div", className:'drop-down-tasks'})
+    divOfTask.style.display = 'none';
+    const addNew = makeElement({type:'button', className:'icon__task_btn', inner:'<svg viewBox="0 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:sketch="http://www.bohemiancoding.com/sketch/ns" fill="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <title>plus</title> <desc>Created with Sketch Beta.</desc> <defs> </defs> <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" sketch:type="MSPage"> <g id="Icon-Set-Filled" sketch:type="MSLayerGroup" transform="translate(-362.000000, -1037.000000)" fill="#afb6c9"> <path d="M390,1049 L382,1049 L382,1041 C382,1038.79 380.209,1037 378,1037 C375.791,1037 374,1038.79 374,1041 L374,1049 L366,1049 C363.791,1049 362,1050.79 362,1053 C362,1055.21 363.791,1057 366,1057 L374,1057 L374,1065 C374,1067.21 375.791,1069 378,1069 C380.209,1069 382,1067.21 382,1065 L382,1057 L390,1057 C392.209,1057 394,1055.21 394,1053 C394,1050.79 392.209,1049 390,1049" id="plus" sketch:type="MSShapeGroup"> </path> </g> </g> </g></svg>'})
+
+
+    //apending
+    divGroupHeader.append(svgDropMenu, textArea, addNew)
+    divGroup.append(divGroupHeader, divOfTask)
+    inProgressContainer.append(divGroup)
+
+    //events
+    svgDropMenu.addEventListener('click', function(e){
+
+        if (divOfTask.style.display === 'none'){
+            svgDropMenu.innerHTML = '<svg viewBox="0 -4.5 24 24" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:sketch="http://www.bohemiancoding.com/sketch/ns" fill="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <title>chevron-down</title> <desc>Created with Sketch Beta.</desc> <defs> </defs> <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" sketch:type="MSPage"> <g id="Icon-Set-Filled" sketch:type="MSLayerGroup" transform="translate(-574.000000, -1201.000000)" fill="#afb6c9"> <path d="M597.405,1201.63 C596.576,1200.8 595.23,1200.8 594.401,1201.63 L586.016,1210.88 L577.63,1201.63 C576.801,1200.8 575.455,1200.8 574.626,1201.63 C573.797,1202.46 573.797,1203.81 574.626,1204.64 L584.381,1215.4 C584.83,1215.85 585.429,1216.05 586.016,1216.01 C586.603,1216.05 587.201,1215.85 587.65,1215.4 L597.405,1204.64 C598.234,1203.81 598.234,1202.46 597.405,1201.63" id="chevron-down" sketch:type="MSShapeGroup"> </path> </g> </g> </g></svg>';
+            divOfTask.style.display = 'flex';
+        } else {
+            svgDropMenu.innerHTML = '<svg viewBox="0 0 20 20" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:sketch="http://www.bohemiancoding.com/sketch/ns" fill="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <title>hamburger</title> <desc>Created with Sketch Beta.</desc> <defs> </defs> <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" sketch:type="MSPage"> <g id="Icon-Set-Filled" sketch:type="MSLayerGroup" transform="translate(-212.000000, -888.000000)" fill="#afb6c9"> <path d="M230,904 L214,904 C212.896,904 212,904.896 212,906 C212,907.104 212.896,908 214,908 L230,908 C231.104,908 232,907.104 232,906 C232,904.896 231.104,904 230,904 L230,904 Z M230,896 L214,896 C212.896,896 212,896.896 212,898 C212,899.104 212.896,900 214,900 L230,900 C231.104,900 232,899.104 232,898 C232,896.896 231.104,896 230,896 L230,896 Z M214,892 L230,892 C231.104,892 232,891.104 232,890 C232,888.896 231.104,888 230,888 L214,888 C212.896,888 212,888.896 212,890 C212,891.104 212.896,892 214,892 L214,892 Z" id="hamburger" sketch:type="MSShapeGroup"> </path> </g> </g> </g></svg>';
+            divOfTask.style.display = 'none';
+        }
+        
+    })
+    textArea.addEventListener('keypress', function(e){
+        if (e.key === 'Enter' && textArea.value !== ''){
+            svgDropMenu.disabled = false;
+            const taskTitle = makeElement({type:'h3', content: textArea.value, className: 'task-title'});
+            textArea.replaceWith(taskTitle);
+            taskTitle.addEventListener('click', () => replaceTaskTilte(taskTitle))
+
+        }
+    })
+    
+}
+
 export {
     dropDown,
-    addNewTask
+    addNewTask,
+    removeAll,
+    createGroup
 }
--- src/main.js
+++ src/main.js
@@ -3,9 +3,13 @@ import '../styles/cards.css';
 import '../styles/utils.css';
 import { dropDown } from './buttons.js';
 import { addNewTask } from './buttons.js';
+import { removeAll } from './buttons.js';
+import { createGroup } from './buttons.js';
 
 //buttons
 const addNewTaskBtn = document.getElementById('newTaskBtn');
+const clearAll = document.getElementById('clearAllBtn');
+const addNewTaskGroupBtn = document.getElementById('addNewTaskGroupBtn');
 
 
 //event Listeners -->
@@ -13,6 +17,7 @@ document.addEventListener('DOMContentLoaded', function(){
     const buttons = document.querySelectorAll('.card__dropDown_btn');
     [...buttons].forEach(btn => btn.querySelector('.icon__header').addEventListener('click', dropDown));
 }) //selects the svg and adds an event listener
-addNewTaskBtn.addEventListener('click', addNewTask)
-
+addNewTaskBtn.addEventListener('click', () => addNewTask('task-wrapper-to-do'))
+clearAll.addEventListener('click', removeAll)
+addNewTaskGroupBtn.addEventListener('click', createGroup)
 
